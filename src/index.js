b481e414469f90a8689ccc3a531138c80eb1c9d1
Jason Antman <antmanjt@gmail.com>
2022-03-25 17:20:46 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,21 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  const todoForm = document.getElementById('create-task-form');
+  todoForm.addEventListener('submit', handlesSubmit)
 });
+
+function handlesSubmit(event) {
+  event.preventDefault();
+
+  const taskList = document.getElementById('tasks');
+  const task = document.createElement('li');
+  task.textContent = event.target.description.value;
+  taskList.append(task);
+
+  const deleteButton = document.createElement('button');
+  deleteButton.textContent = 'X';
+  deleteButton.addEventListener('click', () => {
+});
+task.appen(deleteButton);
+
+event.target.reset();
+}
